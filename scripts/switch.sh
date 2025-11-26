007b64f3ef0bb3a62909e462f2c9f66a01f103df
EC2 Default User <ec2-user@ip-172-31-12-23.us-east-2.compute.internal>
2025-11-26 16:56:55 +0000

    fixes

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 ENV=$1  # blue o green
 
-# Actualizar Nginx con sudo
 NGINX_CONF="/etc/nginx/nginx.conf"
 
-# Reemplazar el puerto del backend según el entorno
 if [ "$ENV" == "blue" ]; then
-    sudo sed -i 's/set \$backend_port .*;/set \$backend_port 8081;/' $NGINX_CONF
+    sudo sed -i 's|proxy_pass http://green;|proxy_pass http://blue;|' $NGINX_CONF
 else
-    sudo sed -i 's/set \$backend_port .*;/set \$backend_port 8082;/' $NGINX_CONF
+    sudo sed -i 's|proxy_pass http://blue;|proxy_pass http://green;|' $NGINX_CONF
 fi
 
-# Reiniciar Nginx
 sudo systemctl restart nginx
 
 echo "Switched to $ENV environment"
-
