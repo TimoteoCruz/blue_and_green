e44d7bb1edfb86f5b21c63804c19eba162bf1165
EC2 Default User <ec2-user@ip-172-31-12-23.us-east-2.compute.internal>
2025-11-26 18:39:28 +0000

    testing rollback

--- app/server.js
+++ app/server.js
@@ -5,6 +5,6 @@ const env = process.env.ENV || 'unknown';
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(`<h1>Blue-Green App - Active Environment: ${env}</h1>`);
+  res.end(`<h1>Blue-Green App - Active Corriendo en: ${env}</h1>`);
 }).listen(PORT, () => console.log(`Server running on port ${PORT} - ENV: ${env}`));
 
